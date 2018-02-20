e1b05445343a33110fcdd380de98984590c04ec3
Joynal Abedin <joynal3483@gmail.com>
2018-02-20 10:54:26 +0600

    ssl ingrated in connection

--- test.js
+++ test.js
@@ -1,8 +1,22 @@
+const fs = require('fs');
 const mongoose = require("mongoose");
 const Visitor = require("./visitor");
 
+const ca = [fs.readFileSync("/opt/mongodb/CA.pem")];
+const key = fs.readFileSync("/opt/mongodb/client.key");
+const cert = fs.readFileSync("/opt/mongodb/client.crt");
+
+const userName = "emailAddress=support@crazyengage.com,CN=*.crazyengage.com,OU=webapp,O=Growthfunnel,L=Dhaka,ST=Dhaka,C=BD";
+
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://127.0.0.1:27017/test");
+mongoose.connect("mongodb://127.0.0.1:27018/growthfunnel", {
+  ssl: true,
+  user: userName,
+  auth: { authMechanism: "MONGODB-X509" },
+  sslCA:ca,
+  sslKey:key,
+  sslCert:cert,
+});
 
 mongoose.connection.on("error", err => {
   console.error(err);
