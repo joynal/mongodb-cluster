7660adaa0bad085c586b69ab7ec0686b78448139
Joynal Abedin <joynal3483@gmail.com>
2018-02-26 18:04:13 +0600

    Mongoose connection refactored

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
-const mongoose = require("mongoose");
 const faker = require("faker");
 const v4 = require("uuid/v4");
-const fs = require('fs');
 const Visitor = require("./visitor");
 
-const ca = [fs.readFileSync("/opt/mongodb/CA.pem")];
-const key = fs.readFileSync("/opt/mongodb/client.key");
-const cert = fs.readFileSync("/opt/mongodb/client.crt");
-
-const userName = "emailAddress=support@crazyengage.com,CN=*.crazyengage.com,OU=webapp,O=Growthfunnel,L=Dhaka,ST=Dhaka,C=BD";
-
-mongoose.Promise = Promise;
-mongoose.connect("mongodb://127.0.0.1:27018/growthfunnel", {
-  ssl: true,
-  user: userName,
-  auth: { authMechanism: "MONGODB-X509" },
-  sslCA:ca,
-  sslKey:key,
-  sslCert:cert,
-});
-
-mongoose.connection.on("error", err => {
-  console.error(err);
-  console.log("MongoDB connection error. Please make sure MongoDB is running.");
-  process.exit();
-});
+require('./connection');
 
 for (let i = 1; i <= 10; i++) {
   let visitor = new Visitor({
--- test.js
+++ test.js
@@ -1,28 +1,6 @@
-const fs = require('fs');
-const mongoose = require("mongoose");
 const Visitor = require("./visitor");
 
-const ca = [fs.readFileSync("/opt/mongodb/CA.pem")];
-const key = fs.readFileSync("/opt/mongodb/client.key");
-const cert = fs.readFileSync("/opt/mongodb/client.crt");
-
-const userName = "emailAddress=support@crazyengage.com,CN=*.crazyengage.com,OU=webapp,O=Growthfunnel,L=Dhaka,ST=Dhaka,C=BD";
-
-mongoose.Promise = Promise;
-mongoose.connect("mongodb://127.0.0.1:27018/growthfunnel", {
-  ssl: true,
-  user: userName,
-  auth: { authMechanism: "MONGODB-X509" },
-  sslCA:ca,
-  sslKey:key,
-  sslCert:cert,
-});
-
-mongoose.connection.on("error", err => {
-  console.error(err);
-  console.log("MongoDB connection error. Please make sure MongoDB is running.");
-  process.exit();
-});
+require('./connection');
 
 Visitor.findOne(
   { _id: "59fc3d4c-5104-47e7-8b6b-c84dedf17136" },
